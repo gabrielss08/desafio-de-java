097357f488032a48b2c72765eca170171bf70007
Gabriel Silva <gabrielss008@gmail.com>
2023-07-09 11:49:51 -0300

    Desafio 5 feito

---
+++ pwc/src/desafio5/Exercicio05ComScanner.java
@@ -0,0 +1,51 @@
+package desafio5;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Exercicio05ComScanner {
+	public static void main(String[] args) {
+		System.out.println("digite uma palavra para achar a substring palindroma mais longa");
+		try (Scanner scanner = new Scanner(System.in)) {
+			String entrada = scanner.nextLine();
+			boolean anagrama = encontrarPalindromo(entrada);
+			if (anagrama) {
+			    System.out.println("algum anagrama de " + entrada + " é um palíndromo.");
+			} else {
+			    System.out.println("Nenhum anagrama de " + entrada + " é um palíndromo.");
+			}
+		}
+    }
+
+    public static boolean encontrarPalindromo(String entrada) {
+        String formatar = entrada.replaceAll("\\s", "").toLowerCase();
+        
+        if (palindromo(formatar)) {
+            return true;
+        }
+
+        HashMap<Character, Integer> contagem = new HashMap<>();
+        
+        for (char c : formatar.toCharArray()) {
+        	contagem.put(c, contagem.getOrDefault(c, 0) + 1);
+        }
+        
+        int j = 0;
+        for (int i : contagem.values()) {//verificar se algum anagrama é palindromo
+            if (i % 2 != 0) {
+                j++;
+            }
+        }
+        return j <= 1;
+    }
+
+    public static boolean palindromo(String entrada) {
+    	String inverter = new StringBuffer(entrada).reverse().toString();
+
+        if(entrada.equals(inverter)) {
+        	return true;
+        }else {
+        	return false;
+        }
+    }
+}
